60a8d01b2d3df3a1a70c93dccab99fb4f4c814a6
Hunter Paulson <hpaulson@my.bcit.ca>
2021-03-29 12:53:53 -0700

    Added new SCSS files

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,27 @@
-// Write your JavaScript here...
+"use strict";
+
+const body = document.body;
+const buttonMenu = document.getElementById('button-menu');
+const navigation = document.getElementById('main-navigation');
+
+buttonMenu.addEventListener('click', openMenu);
+
+buttonMenu.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+});
+
+function openMenu() {
+    body.classList.toggle('show');
+    navigation.classList('activated');
+}
+
+const mql = window.matchMedia('(min-width: 560px');
+
+mql.addEventListener('click', removeTransition(this));
+
+function removeTransition(e) {
+    if(e.matches) {
+        body.classList.remove('show');
+        navigation.classList.remove('activated');
+    }
+}
