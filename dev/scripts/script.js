89aca14faa73cfc9552ac4ff66792182640db537
Hunter Paulson <hpaulson@my.bcit.ca>
2021-03-30 13:39:18 -0700

    Added slideshow features

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,5 +1,6 @@
 // "use strict";
 
+//Hamburger Menu 
 const body = document.body;
 const buttonMenu = document.getElementById('button-menu');
 const navigation = document.getElementById('main-navigation');
@@ -26,15 +27,57 @@ function removeTransition(e) {
     }
 }
 
-// const menuBtn = document.querySelector('.menu-btn');
-// let menuOpen = false;
 
-// menuBtn.addEventListener('click', () => {
-//     if(!menuOpen) {
-//         menuBtn.classList.add('open');
-//         menuOpen = true;
-//     } else {
-//         menuBtn.classList.remove('open');
-//         menuOpen = false;
+//Header Slideshow
+
+// var dots = document.getElementsByClassName("dot");
+
+// function showSlides(dots) {
+//     let slideIndex = 0;  
+//     let slides = document.getElementsByClassName("mySlides");
+    
+
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//         console.log('sliding');
 //     }
-// });
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}
+//     for (let i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace("active", "");
+//         console.log('slidinggggggggggg');
+//     }
+//     slides[slideIndex-1].className.display = "block";
+//     dots[slideIndex-1].className += "active";
+//     setTimeout(showSlides(dots), 3000);
+// }
+
+// showSlides(dots);
+
+let slideIndex = 1;
+showSlides();
+
+function showSlides() {
+
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000);
+}
+
+function plusSlides (n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide (n) {
+    showSlides(slideIndex = n);
+}
